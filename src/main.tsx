02c5e268cb53813d4f83c7f2ec72ea6891b2ebdf
S-Irie <irieshungo10710@gmail.com>
2024-05-03 20:04:31 +0900

    feat: font setting

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/reset.css";
+import "./styles/global.css";
 
 const rootElement = document.getElementById("root");
 
