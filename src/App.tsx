80773de826ca1c716e2cf50daf4a34f651ffd6c8
S-Irie <irieshungo10710@gmail.com>
2024-05-03 21:06:43 +0900

    feat: create template

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-const App = () => {
-  return <div>Hello</div>;
+import type { FC } from "react";
+import styles from "./App.module.scss";
+import DrawWindow from "./components/DrawWindow";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const App: FC = () => {
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+      <DrawWindow />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
